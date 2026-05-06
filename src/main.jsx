4010ef47d3aac9ea9b15131749357163f9e494fa
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2026-05-06 08:47:21 +0200

    Added css files.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+
+import "./styles/base/Reset.css";
+import "./styles/base/Fonts.css";
+import "./styles/base/Spacing.css";
+import "./styles/base/Typography.css";
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -8,5 +14,5 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
