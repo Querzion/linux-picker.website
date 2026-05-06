63d25714580bef78cb4626d5ebdc72377074d054
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2026-05-06 08:47:10 +0200

    Updated the routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,21 @@
-import { Routes, Route } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import MainLayout from './assets/layouts/MainLayout'
-import './App.css'
-import MainPage from './assets/pages/MainPage'
-import AboutPage from './assets/pages/AboutPage'
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./assets/layouts/MainLayout";
+
+import MainPage from "./assets/pages/MainPage";
+import AboutPage from "./assets/pages/AboutPage";
+
+import "./App.css";
 
 function App() {
-
-  return (
-    <MainLayout selectedDistro={selectedDistro}>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <MainPage
-              distros={distros}
-              selectedDistro={selectedDistro}
-              setSelectedDistro={setSelectedDistro}
-            />
-          }
-        />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
-    </MainLayout>
-  )
+    return (
+        <Routes>
+            {/* Layout route */}
+            <Route element={<MainLayout />}>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/about" element={<AboutPage />} />
+            </Route>
+        </Routes>
+    );
 }
 
-export default App
+export default App;
